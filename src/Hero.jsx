afc2681c47eb1fe6df8e2adf3bde0efc72c51bd7
ayyyub777 <140520247+ayyyub777@users.noreply.github.com>
2023-08-11 18:33:26 +0100

    Update Hero.jsx

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -10,12 +10,15 @@ function Hero() {
       </div>
       <div className="title my-14 mt-8">
         <h1 className="text-3xl font-bold">Hi! I'm Ayoub Bigharassine</h1>
-        <h2 className="mb-6 text-xl">ReactJs Developer</h2>
+        <h2 className="mb-6 text-xl">Software Developer</h2>
         <h2 className="text-lg text-gray sm:text-xl">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse
-          sapiente corporis cupiditate inventore et nobis autem! Fuga sequi
-          quasi perferendis non accusamus, ex, tempore ea minima maxime delectus
-          culpa quibusdam?
+          An enthusiastic and adaptable software developer who loves creating
+          smart and effective solutions for everyday problems. I have a solid
+          grasp of different programming languages and a strong attention to
+          detail. My expertise lies in transforming intricate concepts into
+          user-friendly applications. My portfolio showcases my personal
+          projects, demonstrating my skills, creativity, and commitment to
+          pushing boundaries.
         </h2>
       </div>
     </div>
